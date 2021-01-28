d12b463ab7076537de47793ea7ed0ccc545da6a9
zebedal <zebedal@bitbucket.org>
2021-01-28 13:47:20 +0000

    broken homepage into smaller components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from  '../../components/directory/directory';
+
+ const Homepage = () => 
+    
+    <div className="homepage">
+        <Directory></Directory>
+    </div>
+    
+
+
+export default Homepage;
